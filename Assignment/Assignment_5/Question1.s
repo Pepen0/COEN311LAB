689c724fa03db99b531b0fcd55d7cba66e311884
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-12-04 15:20:29 -0500

    Update Question1.s

--- Assignment/Assignment_5/Question1.s
+++ Assignment/Assignment_5/Question1.s
@@ -23,23 +23,32 @@ str2: .asciz "World!"
 _start:
     ldr r0, =str1   
     ldr r1, =str2  
-    bl STRCAT      
+    // first call
+    bl STRCAT    
+    //will result in str1 = "Hello World!"  
+    //Will result in str2 = "World!"   
     
     // second call 
-    bl STRCAT
+    bl STRCAT 
+    //will result in str1 = "Hello World!World!"
+    //Will result in str2 = "World!"  
+
     b .           
 
 STRCAT:
     push {lr}     
 
+    // find end of str1
     find_end_str1:
         ldrb r2, [r0]  
         adds r0, r0, #1 
         cmp r2, #0      
     bne find_end_str1 
 
+    // decrement r0 to point to the last character of str1
     subs r0, r0, #1  
 
+    // copy str2 to str1
     copy_str2:
         ldrb r2, [r1], #1 
         strb r2, [r0], #1  
