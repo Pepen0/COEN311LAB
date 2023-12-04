cfcfd2548d34b7889a90de2dbcc721410123c0d1
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-12-04 18:10:26 -0500

    doubled check questions

--- Assignment/Assignment_5/Question1.s
+++ Assignment/Assignment_5/Question1.s
@@ -1,14 +1,14 @@
-// Assignment number 5      
-// CPU architectures       
-// Penoelo Thibeaud, student id: 40212017
+@ Assignment number 5      
+@ CPU architectures       
+@ Penoelo Thibeaud, student id: 40212017
 
-//Question 1)
-//Write a subroutine STRCAT to concatenate two strings. 
-//The main program passes the addresses of both the strings by registers to the subroutine. 
-//The second string is concatenated to the first string. 
-//Note to reserve extra memory space after the first string to hold the resulting concatenated string. 
-//The subroutine STRCAT is to be called twice by the main program. 
-//in Arm assembly language
+@ Question 1)
+@ Write a subroutine STRCAT to concatenate two strings. 
+@ The main program passes the addresses of both the strings by registers to the subroutine. 
+@ The second string is concatenated to the first string. 
+@ Note to reserve extra memory space after the first string to hold the resulting concatenated string. 
+@ The subroutine STRCAT is to be called twice by the main program. 
+@ in Arm assembly language
 
 .syntax unified
 .cpu cortex-m4
@@ -23,32 +23,32 @@ str2: .asciz "World!"
 _start:
     ldr r0, =str1   
     ldr r1, =str2  
-    // first call
+    @ first call
     bl STRCAT    
-    //will result in str1 = "Hello World!"  
-    //Will result in str2 = "World!"   
+    @ will result in str1 = "Hello World!"  
+    @ Will result in str2 = "World!"   
     
-    // second call 
+    @ second call 
     bl STRCAT 
-    //will result in str1 = "Hello World!World!"
-    //Will result in str2 = "World!"  
+    @ will result in str1 = "Hello World!World!"
+    @ Will result in str2 = "World!"  
 
     b .           
 
 STRCAT:
     push {lr}     
 
-    // find end of str1
+    @ find end of str1
     find_end_str1:
         ldrb r2, [r0]  
         adds r0, r0, #1 
         cmp r2, #0      
     bne find_end_str1 
 
-    // decrement r0 to point to the last character of str1
+    @ decrement r0 to point to the last character of str1
     subs r0, r0, #1  
 
-    // copy str2 to str1
+    @ copy str2 to str1
     copy_str2:
         ldrb r2, [r1], #1 
         strb r2, [r0], #1  
--- Assignment/Assignment_5/Question2.s
+++ Assignment/Assignment_5/Question2.s
@@ -1,63 +1,50 @@
-// Assignment number 5      
-// CPU architectures       
-// Penoelo Thibeaud, student id: 40212017
+@  Assignment number 5      
+@  CPU architectures       
+@  Penoelo Thibeaud, student id: 40212017
 
-//Write a complete ARM assembly program to implement bubble sort algorithm of sorting a list of 
-//n words stored consecutively in memory locations starting from ARRAY. 
-//The program should include writing a subroutine bubble_sort whose input parameters 
-//(n, ARRAY) are passed from the main program using the stack. 
-//Use the bubble_sort algorithm given below:
-//for(i = n-1; i > 0; i--) {
-//  for(j = 1; j <= i; j++) {
-//if(array[j-1] > array[j]){     //swap them    temp = array[j-1];    array[j-1] = array[j];    array[j] = temp; }
-//  } }
-//in arm assembly language
+@ Write a complete ARM assembly program to implement bubble sort algorithm of sorting a list of 
+@ n words stored consecutively in memory locations starting from ARRAY. 
+@ The program should include writing a subroutine bubble_sort whose input parameters 
+@ (n, ARRAY) are passed from the main program using the stack. 
+@ Use the bubble_sort algorithm given below:
+@ for(i = n-1; i > 0; i--) {
+@   for(j = 1; j <= i; j++) {
+@ if(array[j-1] > array[j]){     //swap them    temp = array[j-1];    array[j-1] = array[j];    array[j] = temp; }
+@   } }
+@ in arm assembly language
 
 .syntax unified
 .cpu cortex-m4
 .thumb
-.word 0x20000400 
-.word 0x800000ed 
-.space 0xe4 
 
 .data
-array: 
-.word 0x00000001
-.word 0x00000002
-.word 0x00000003
-.word 0x00000004
-.word 0x00000005
-.word 0x00000006
-.word 0x00000007
-.word 0x00000008
-.word 0x00000009
-.word 0x0000000a
+array: .word 2, 3, 5, 7, 10, 11, 13, 17, 19, 0
 
 .text
 start:
     ldr r0, =array  
-    mov r1, #10     
+    mov r1, #10     @ r1 is used as n 
     bl bubble_sort  
     b .            
 
 bubble_sort:
     push {r4-r7, lr} 
 
-    mov r4, r0       
-    mov r5, r1        
+    mov r4, r0     @ r4 is used as ARRAY  
+    mov r5, r1     @ r5 is used as i   
 
 outer_loop:
-    mov r6, #0       
+    mov r6, #0    @ r6 is used as j-1   
 
 inner_loop:
     cmp r6, r5        
     bge outer_loop_end
 
-    // Load current and next elements
-    ldr r0, [r4, r6, lsl #2]
-    ldr r1, [r4, r6, lsl #2]!
+    @ Load current and next elements
+    ldr r0, [r4, r6, lsl #2]   
+    ldr r1, [r4, r6, lsl #2]!  
 
-    // Compare and swap if needed
+    @ Compare and swap if needed
     cmp r0, r1
     blt no_swap
     str r1, [r4, r6, lsl #2]
